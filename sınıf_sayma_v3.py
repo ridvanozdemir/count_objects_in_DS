272f81ce427e1cbdd663efdfc5a195d707ce5989
ridvanozdemir <66924366+ridvanozdemir@users.noreply.github.com>
2021-09-18 22:16:29 +0300

    Update sınıf_sayma_v3.py

--- sınıf_sayma_v3.py
+++ sınıf_sayma_v3.py
@@ -5,28 +5,13 @@ Created on Thu Jul 15 02:49:11 2021
 @author: ridvan.ozdemir
 
 v2:new code is way much shorter
+v3:looks nicer
 """
 
 import os
 import glob
 
-#path = 'D:/ridvan_2020/01_gelisim/python_ornekleri/furkan'
-#path = 'E:/Yolo_v4/darknet/build/darknet/x64/data/test'
 path = "E:/ridvan_2021/05_PhD/tik_ler/tik_4/R_F_D_v4/"
-#D:\ridvan_2020\05_PhD\YOLO\tik_2\yedekler\R_F_D_Bigger_v2_09
-#D:\ridvan_2020\05_PhD\YOLO\tik_2\yedekler\First_Plus_07
-
-"""
-sleeper = 0
-bolt= 0
-head_check= 0
-fastening= 0
-rail_crack= 0
-fastening_rotated_deformed= 0
-corrugation= 0
-squat= 0
-surface_defect= 0
-"""
 
 number_of_pruducts={}
 
